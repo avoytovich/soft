e68c08b343bfe92455a5b65969441bc0d5b0a002
avoytovich <levanwork@gmail.com>
2016-04-14 21:24:46 +0000

    Initial commit

--- script.js
+++ script.js
@@ -2,18 +2,17 @@
 $(document).ready(function() {
     $('#button_about_me').click(function() {
         $('#about_me').show();
-        $('.background').css('opacity','0.5');
-        $('#about_me').animate({top: '200px', left: '300px',  width: '700px', height: '390px'}, 'slow');
+        $('.background').css('opacity','0.1');
+        $('#about_me').animate({top: '100px', left: '200px',  width: '900px', height: '700px'}, 'slow');
         $('.name').show('slow');
-        $('.goal').show('slow');
     });
 });
 
 $(document).ready(function() {
     $('#button_useful_links').click(function() {
         $('#useful_links').show();
-        $('.background').css('opacity','0.5');
-        $('#useful_links').animate({top: '200px', left: '300px',  width: '700px', height: '390px'}, 'slow');
+        $('.background').css('opacity','0.1');
+        $('#useful_links').animate({top: '100px', left: '200px',  width: '900px', height: '700px'}, 'slow');
         $('#often').show('slow');
     });
 });
@@ -22,7 +21,7 @@ $(document).ready(function(){
     $('#button_about_me_close').click(function() {
         $('#about_me').animate({top: '100px', left: '200px',  width: '0px', height: '0px'}, 'fast');
         $('#about_me').hide();
-        $('.name, .goal').hide();
+        $('.name').hide();
         $('#html, #css, #javascript, #jQuery, #bootstrap').css('width','0%');
         $('#html, #css, #javascript, #jQuery, #bootstrap').html('');
         $('.background').css('opacity','1');
@@ -31,8 +30,10 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $('#button_useful_links_close').click(function() {
-        $('#useful_links').animate({top: '500px', right: '200px',  width: '0px', height: '0px'}, 'fast');
+        $('#useful_links').animate({top: '500px', left: '1000px',  width: '0px', height: '0px'}, 'fast');
         $('#useful_links').hide();
+        $('#html, #css, #javascript, #jQuery, #bootstrap').css('width','0%');
+        $('#html, #css, #javascript, #jQuery, #bootstrap').html('');
         $('.background').css('opacity','1');
     });
 });
@@ -44,11 +45,11 @@ $(document).ready(function() {
         $('#css').animate({width: '250px'}, 'slow');
         $('#css').html('CSS');
         $('#javascript').animate({width: '200px'}, 'slow');
-        $('#javascript').html('javaScript');
+        $('#javascript').html('JavaScript');
         $('#jQuery').animate({width: '200px'}, 'slow');
         $('#jQuery').html('jQuery');
         $('#bootstrap').animate({width: '200px'}, 'slow');
-        $('#bootstrap').html('bootstrap');
+        $('#bootstrap').html('Bootstrap');
     });
 })
 
