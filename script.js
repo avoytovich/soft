a9b28733a9e02b383eba26f26675cdf7483f0339
avoytovich <levanwork@gmail.com>
2016-04-17 19:02:30 +0000

    Initial commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('#button_about_me').click(function() {
         $('#about_me').show();
         $('.background').css('opacity','0.3');
-        $('#about_me').animate({top: '100px', left: '200px',  width: '900px', height: '700px'}, 'slow');
+        $('#about_me').animate({top: '400px', left: '200px',  width: '900px', height: '700px'}, 'slow');
         $('.name').show('slow');
         $(".p_one").fadeIn('slow');
         $(".p_two").fadeIn(1000);
@@ -19,14 +19,14 @@ $(document).ready(function() {
     $('#button_useful_links').click(function() {
         $('#useful_links').show();
         $('.background').css('opacity','0.3');
-        $('#useful_links').animate({top: '100px', left: '200px',  width: '900px', height: '700px'}, 'slow');
+        $('#useful_links').animate({top: '400px', left: '200px',  width: '900px', height: '700px'}, 'slow');
         $('#often').show('slow');
     });
 });
 
 $(document).ready(function(){
     $('#button_about_me_close').click(function() {
-        $('#about_me').animate({top: '100px', left: '200px',  width: '0px', height: '0px'}, 'fast');
+        $('#about_me').animate({top: '400px', left: '200px',  width: '0px', height: '0px'}, 'fast');
         $('#about_me').hide();
         $('.name').hide();
         $('#html, #css, #javascript, #jQuery, #bootstrap').css('width','0%');
@@ -38,7 +38,7 @@ $(document).ready(function(){
 
 $(document).ready(function(){
     $('#button_useful_links_close').click(function() {
-        $('#useful_links').animate({top: '500px', left: '1000px',  width: '0px', height: '0px'}, 'fast');
+        $('#useful_links').animate({top: '500px', left: '200px',  width: '0px', height: '0px'}, 'fast');
         $('#useful_links').hide();
         $('#html, #css, #javascript, #jQuery, #bootstrap').css('width','0%');
         $('#html, #css, #javascript, #jQuery, #bootstrap').html('');
